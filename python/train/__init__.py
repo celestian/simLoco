5698391cf1f4ec026a15061c114216de64bf2a36
celestian <petr.celestian@gmail.com>
2012-07-15 12:36:45 +0200

    new directory layout

--- python/train/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/bin/env python3.2
-# coding=utf-8
-
-
-__all__ = ['drive','route']
--- python/train/vehicle.py
+++
@@ -1,5 +0,0 @@
-#!/bin/env python3.2
-# coding=utf-8
-
-
-# ToDo: Classes for manipulating database of rail vehicles
