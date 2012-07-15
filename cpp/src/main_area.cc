b1ee1288e1a9f5474622ca5f0d2c3df183e666d7
celestian <petr.celestian@gmail.com>
2012-07-15 12:16:47 +0200

    only code from cluttermm tutorial, good, everything works

--- cpp/src/main_area.cc
+++ cpp/src/main_area.cc
@@ -1,13 +1,37 @@
-#include <cstdio>
-#include "area.hh"
+#include <cluttermm.h>
 
-using namespace std;
+int main(int argc, char** argv)
+{
+  Clutter::init(&argc, &argv);
 
+  // Get the stage and set its size and color:
+  const Glib::RefPtr<Clutter::Stage> stage = Clutter::Stage::get_default();
+  stage->set_size(200, 200);
+  stage->set_color(Clutter::Color(0, 0, 0, 255)); // black
 
-int main(int argc, char **argv) {
+  Clutter::Color actor_color (0xff, 0xff, 0xff, 0x99);
 
-	Area area(argv[1]);
+  // Add a rectangle to the stage:
+  const Glib::RefPtr<Clutter::Rectangle> rect = Clutter::Rectangle::create(actor_color);
+  rect->set_size(100, 2);
+  rect->set_position(20, 20);
+  stage->add_actor(rect);
+  rect->show();
 
-	return 0;
+  // Add a label to the stage:
+  const Glib::RefPtr<Clutter::Actor> label =
+    Clutter::Text::create("Sans 12", "Some Text", actor_color);
+  label->set_size(500, 500);
+  label->set_position(20, 150);
+  stage->add_actor(label);
+  label->show();
+
+  // Show the stage:
+  stage->show();
+
+  // Start the main loop, so we can respond to events:
+  Clutter::main();
+
+  return 0;
 }
 
