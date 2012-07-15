6bd7ba0a509b10079c9b3cba2cb65a36fd94afae
celestian <petr.celestian@gmail.com>
2012-07-15 12:17:23 +0200

    little work

--- cpp/src/track.cc
+++ cpp/src/track.cc
@@ -1,1 +1,29 @@
 #include "track.hh"
+#include <iostream>
+
+using namespace std;
+
+
+Track::Track (const string & descp) {
+	
+	id = stoi(descp.substr(4, 4));
+	type = descp.at(2);
+	
+	if (type == 'r') {
+		title = "rail xxxx";
+		lenght = 800;
+	}
+	if (type == 'e') {
+		title = "switch xxxx";
+		lenght = 0;
+	}
+	title.replace(title.find("xxxx"), 4, to_string(id) );
+	
+	back_left = 0;
+	back_direct = 0;
+	back_right = 0;
+
+	forward_left = 0;
+	forward_direct = 0;
+	forward_right = 0;
+}
--- cpp/src/track.hh
+++ cpp/src/track.hh
@@ -1,7 +1,39 @@
 #ifndef _TRACK_H_
 #define _TRACK_H_
 
+#include <string>
+#include <vector>
+#include <iostream>
 
 
+class Track {
+
+	public:
+	
+		Track (const std::string & descp);
+		int getId () { return id; };
+		void print () {	 printf("%s\t|  %i\t%c\t%i\t%i\t%i\t%i\t%i\t%i\t%i\n", title.c_str(), id, type, lenght, back_left, back_direct, back_right, forward_left, forward_direct, forward_right); };
+		
+		int back_left;
+		int	back_direct;
+		int	back_right;
+
+		int	forward_left;
+		int	forward_direct;
+		int forward_right;
+
+
+		
+	private:
+		
+		int id;
+		char type;
+		std::string title;
+		int lenght;
+
+
+
+};
 
 #endif // _TRACK_H_
+
